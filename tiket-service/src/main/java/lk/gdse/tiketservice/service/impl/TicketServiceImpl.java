105a510b5409534640ca32260fcd835d7f426b24
MavithaShehar <mavithashehar@gmail.com>
2024-07-01 22:30:59 +0530

    update and change TicketServiceImpl

--- tiket-service/src/main/java/lk/gdse/tiketservice/service/impl/TicketServiceImpl.java
+++ tiket-service/src/main/java/lk/gdse/tiketservice/service/impl/TicketServiceImpl.java
@@ -23,46 +23,34 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public String saveTicket(TicketDTO ticketDTO) {
-
         if (ticketRepo.existsById(ticketDTO.getTicketNo())) {
             return VarList.RSP_DUPLICATED;
+        }
+
+        if (isValidUserAndVehicle(ticketDTO)) {
+            ticketRepo.save(modelMapper.map(ticketDTO, Ticket.class));
+            return VarList.RSP_SUCCESS;
         } else {
-
-            String userId = restTemplate.getForObject("http://localhost:8080/api/v1/user/search/" + ticketDTO.getUserId(), String.class);
-            String vehicleNO = restTemplate.getForObject("http://localhost:8080/api/v1/vehicle/search/" + ticketDTO.getVehicleNo(), String.class);
-
-            if (userId != null && userId.equals(ticketDTO.getUserId()) && vehicleNO != null && vehicleNO.equals(ticketDTO.getVehicleNo())) {
-                ticketRepo.save(modelMapper.map(ticketDTO, Ticket.class));
-                return VarList.RSP_SUCCESS;
-            } else {
-
-                return VarList.RSP_NO_DATA_FOUND;
-            }
+            return VarList.RSP_NO_DATA_FOUND;
         }
     }
 
 
-
     @Override
     public String updateTicket(TicketDTO ticketDTO) {
-
-        if (ticketRepo.existsById(ticketDTO.getTicketNo())){
-
-            String userId = restTemplate.getForObject("http://localhost:8080/api/v1/user/search/" + ticketDTO.getUserId(), String.class);
-            String vehicleNO = restTemplate.getForObject("http://localhost:8080/api/v1/vehicle/search/" + ticketDTO.getVehicleNo(), String.class);
-
-            if (userId != null && userId.equals(ticketDTO.getUserId()) && vehicleNO != null && vehicleNO.equals(ticketDTO.getVehicleNo())) {
-                ticketRepo.save(modelMapper.map(ticketDTO, Ticket.class));
-                return VarList.RSP_SUCCESS;
-            } else {
-
-                return VarList.RSP_NO_DATA_FOUND;
-            }
-
-        }else {
+        if (ticketRepo.existsById(ticketDTO.getTicketNo()) && isValidUserAndVehicle(ticketDTO)) {
+            ticketRepo.save(modelMapper.map(ticketDTO, Ticket.class));
+            return VarList.RSP_SUCCESS;
+        } else {
             return VarList.RSP_NO_DATA_FOUND;
         }
+    }
 
+    private boolean isValidUserAndVehicle(TicketDTO ticketDTO) {
+        String userId = restTemplate.getForObject("http://localhost:8080/api/v1/user/search/" + ticketDTO.getUserId(), String.class);
+        String vehicleNo = restTemplate.getForObject("http://localhost:8080/api/v1/vehicle/search/" + ticketDTO.getVehicleNo(), String.class);
+
+        return userId != null && userId.equals(ticketDTO.getUserId()) && vehicleNo != null && vehicleNo.equals(ticketDTO.getVehicleNo());
     }
 
     @Override
