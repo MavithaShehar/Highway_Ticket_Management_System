a3e9fe3211c4e8b2704b1e5653058f4f85224624
MavithaShehar <mavithashehar@gmail.com>
2024-07-05 02:14:04 +0530

    User validation

--- user-service/src/main/java/lk/gdse/userservice/entity/User.java
+++ user-service/src/main/java/lk/gdse/userservice/entity/User.java
@@ -3,6 +3,7 @@ package lk.gdse.userservice.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,10 +17,16 @@ import lombok.NoArgsConstructor;
 @Builder
 public class User {
     @Id
+    @NotBlank(message = "User ID is NO be Empty")
     private String userId;
+    @NotBlank(message = "Name is NO be Empty")
     private String userName;
+    @NotBlank(message = "Password is NO be Empty")
     private String password;
+    @NotBlank(message = "Email is NO be Empty")
     private String email;
+    @NotBlank(message = "Phone Number is NO be Empty")
     private String phone;
+    @NotBlank(message = "Address is NO be Empty")
     private String address;
 }
