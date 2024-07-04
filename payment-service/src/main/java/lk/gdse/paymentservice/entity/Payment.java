2eb2067617234b46393d5f7f38eafa3b23228a8b
MavithaShehar <mavithashehar@gmail.com>
2024-07-05 02:24:06 +0530

    Payment validation

--- payment-service/src/main/java/lk/gdse/paymentservice/entity/Payment.java
+++ payment-service/src/main/java/lk/gdse/paymentservice/entity/Payment.java
@@ -3,6 +3,7 @@ package lk.gdse.paymentservice.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,12 +19,15 @@ import java.util.Date;
 public class Payment {
 
     @Id
+    @NotBlank(message = "payment No is Not Be Null")
     private String paymentNo;
     private Date date;
     private double price;
     private double given_Amount;
     private double remaining_Amount;
     private String ticketNo;
+    @NotBlank(message = "User Id No is Not Be Null")
     private String userId;
+    @NotBlank(message = "Vehicle No Id No is Not Be Null")
     private String vehicleNo;
 }
