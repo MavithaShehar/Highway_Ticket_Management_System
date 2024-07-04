b0fe0707427e270c45ce6c277499570de2f80698
MavithaShehar <mavithashehar@gmail.com>
2024-07-05 02:21:32 +0530

    Ticket validation

--- tiket-service/src/main/java/lk/gdse/tiketservice/entity/Ticket.java
+++ tiket-service/src/main/java/lk/gdse/tiketservice/entity/Ticket.java
@@ -3,9 +3,11 @@ package lk.gdse.tiketservice.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.Date;
 
@@ -17,18 +19,23 @@ import java.util.Date;
 public class Ticket {
 
     @Id
+    @NotNull(message = "Ticket No cannot be null")
     private String ticketNo;
 
     private Date date;
+    @NotNull(message = "Price No cannot be null")
     private double price;
 
+    @NotNull(message = "From Where No cannot be null")
     private String fromWhere;
 
+    @NotNull(message = "To Where No cannot be null")
     private String toWhere;
 
-
+    @NotNull(message = "Vehicle No  cannot be null")
     private String vehicleNo;
 
+    @NotNull(message = "User Id No cannot be null")
     private String userId;
 
 }
